4cb79c159e7b815096755db51d8a57339098894b
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2023-01-06 12:04:26 +0100

    Remove directed pharmacophore option in create_hash_db.py

--- create_hash_db.py
+++ create_hash_db.py
@@ -3,20 +3,18 @@
 import argparse
 import os
 import sqlite3
-import sys
 from functools import partial
 from itertools import combinations
 from multiprocessing import Pool
 
-import numpy as np
-from pmapper.pharmacophore import Pharmacophore as P
-from pmapper.customize import load_smarts
+from pmapper.utils import load_multi_conf_mol
+from pmapper.utils import load_smarts
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
+from read_input import read_input
 
 smarts = load_smarts()
-smarts["T"] = (Chem.MolFromSmarts('[#0]'),)
 
 
 def create_db(fname):
@@ -34,17 +32,16 @@ def create_db(fname):
 
 def read_smi(fname, dbname):
     with sqlite3.connect(dbname) as con:
-        with open(fname) as f:
-            for line in f:
-                smi = line.strip().split()[0]
-                if not con.execute("SELECT EXISTS(SELECT 1 FROM frags WHERE smi = ?)", (smi, )).fetchone()[0]:
-                    yield smi
+        for mol, title in read_input(fname):
+            if not con.execute("SELECT EXISTS(SELECT 1 FROM frags WHERE smi = ?)", (title, )).fetchone()[0]:
+                yield mol
 
 
-def process_smi(smi, nconf, seed, binstep, min_features, max_features, directed):
-    mol = gen_confs(mol=Chem.MolFromSmiles(smi), nconf=nconf, seed=seed)
-    hashes = gen_hashes(mol=mol, binstep=binstep, min_features=min_features, max_features=max_features, directed=directed)
-    return smi, hashes
+def process_smi(mol, nconf, seed, binstep, tolerance, min_features, max_features):
+    if len(mol.GetConformers()) == 1:
+        mol = gen_confs(mol=mol, nconf=nconf, seed=seed)
+    hashes = gen_hashes(mol=mol, binstep=binstep, tolerance=tolerance, min_features=min_features, max_features=max_features)
+    return mol.GetProp("_Name"), hashes
 
 
 def gen_confs(mol, nconf, seed):
@@ -53,29 +50,7 @@ def gen_confs(mol, nconf, seed):
     return mol
 
 
-def get_complementary_point_xyz(mol, conf_id, start_atom_id, binstep):
-    """
-    Find coordinates of a point on a distance of a bin step in the direction of the neighboring atom to
-    the attachment point.
-    :param mol:
-    :param conf_id:
-    :param start_atom_id: id of the attachment point (atom with atomic number 0)
-    :param binstep:
-    :return: tuple of xyz coordinates
-    """
-    try:
-        end_atom_id = mol.GetAtomWithIdx(start_atom_id).GetNeighbors()[0].GetIdx()
-        conf = mol.GetConformer(conf_id)
-        a = conf.GetAtomPosition(start_atom_id)
-        b = conf.GetAtomPosition(end_atom_id)
-        output_pos = a + (b - a) / np.linalg.norm(a - b) * binstep
-    except Exception as e:
-        sys.stderr.write(Chem.MolToSmiles(mol) + '\n' + str(e))
-        return tuple()
-    return tuple(output_pos)
-
-
-def gen_hashes(mol, binstep, min_features, max_features, directed):
+def gen_hashes(mol, binstep, tolerance, min_features, max_features):
     """
     Attachment point features will be always present in output combination. Thus, min_features is the minimum number of
     added other features, max_features is correspondingly the maximum number of other features.
@@ -91,56 +66,33 @@ def gen_hashes(mol, binstep, min_features, max_features, directed):
     :return:
     """
     hashes = []
-    p = P(cached=True, bin_step=binstep)
-    atom_ids = p._get_features_atom_ids(mol, smarts)
-
-    for conf in mol.GetConformers():
-
-        conf_id = conf.GetId()
-        p.load_from_atom_ids(mol, atom_ids, conf_id)
-
-        feature_ids = p.get_feature_ids()
-        att_id = feature_ids['T'][0]
-        other_ids = []
-        for k, v in feature_ids.items():
-            if k != 'T':
-                other_ids.extend(v)
-
-        if directed:
-            # determine coordinates of the end of the attachment point feature and add it to pharmacophore
-            end_xyz = get_complementary_point_xyz(mol, conf_id, att_id, binstep)
-            if not end_xyz:
-                continue
-            end_id = p.add_feature('Q', end_xyz)
-            fixed_ids = [att_id, end_id]
-        else:
-            fixed_ids = [att_id]
-
-        max_features = min(max_features, len(other_ids))
-        for n in range(min_features, max_features + 1):
-            for comb in combinations(other_ids, n):
-                hashes.append(p.get_signature_md5(ids=fixed_ids + list(comb)))
-
+    for p in load_multi_conf_mol(mol, smarts_features=smarts, bin_step=binstep, cached=True):
+        feature_ids = list(range(sum(p.get_features_count().values())))
+        if min_features > len(feature_ids):
+            return
+        for n in range(min_features, min(max_features + 1, len(feature_ids))):
+            for comb in combinations(feature_ids, n):
+                hashes.append(p.get_signature_md5(ids=list(comb), tol=tolerance))
     return tuple(set(hashes))
 
 
 def main():
     parser = argparse.ArgumentParser(description='Generate database of 3D pharmacophore hashes of fragments having '
-                                                 'one attachemnt point.')
+                                                 'one attachment point.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-i', '--input', metavar='FILENAME', required=True,
                         help='SMILES files. No header.')
     parser.add_argument('-o', '--output', metavar='FILENAME', required=True,
                         help='SQLite3 DB file.')
     parser.add_argument('-b', '--binstep', metavar='NUMERIC', required=False, type=float, default=1.5,
                         help='binning step to generate 3D pharmacophore hashes.')
-    parser.add_argument('-d', '--directed', required=False, action='store_true', default=False,
-                        help='if set attachment points will be considered as directed features.')
+    parser.add_argument('-tol', '--tolerance', metavar='NUMERIC', type=int, default=0,
+                        help='tolerance used for calculation of a stereoconfiguration sign')
     parser.add_argument('-n', '--nconf', metavar='INTEGER', required=False, type=int, default=50,
                         help='number of conformers generated for each input fragment.')
     parser.add_argument('-s', '--seed', metavar='INTEGER', required=False, type=int, default=0,
-                        help='random seed. Default: 0.')
+                        help='random seed.')
     parser.add_argument('-c', '--ncpu', metavar='INTEGER', required=False, type=int, default=1,
-                        help='number of cpu cores to use. Default: 1.')
+                        help='number of cpu cores to use.')
 
     Chem.SetDefaultPickleProperties(Chem.PropertyPickleOptions.AllProps)
 
@@ -158,13 +110,21 @@ def main():
                                                                   nconf=args.nconf,
                                                                   seed=args.seed,
                                                                   binstep=args.binstep,
-                                                                  min_features=1,
-                                                                  max_features=5,
-                                                                  directed=args.directed),
+                                                                  tolerance=args.tolerance,
+                                                                  min_features=2,
+                                                                  max_features=6),
                                                           read_smi(args.input, args.output)), 1):
-        smi_id = list(cur.execute("INSERT OR IGNORE INTO frags(smi) VALUES(?) RETURNING id", (smi, )))[0][0]
-        cur.executemany("INSERT INTO hashes(id, hash) VALUES(?, ?)", [(smi_id, h)for h in hashes])
-        con.commit()
+        if not hashes:
+            continue
+        # print(i, smi, hashes)
+        try:
+            smi_id = list(cur.execute("INSERT OR IGNORE INTO frags(smi) VALUES(?) RETURNING id", (smi, )))[0][0]
+            cur.executemany("INSERT INTO hashes(id, hash) VALUES(?, ?)", [(smi_id, h) for h in hashes])
+            con.commit()
+        except IndexError:
+            print(i, smi, len(hashes))
+            print('-')
+            # print(smi, list(cur.execute("INSERT OR IGNORE INTO frags(smi) VALUES(?) RETURNING id", (smi, ))))
 
     sql = "CREATE INDEX hashes_hash_idx ON hashes(hash)"
     con.execute(sql)
